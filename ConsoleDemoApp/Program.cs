714a8be10110a705816f120beff198e19fc1bda5
aajay007 <40169124+aajay007@users.noreply.github.com>
2020-08-27 18:22:19 +0530

    initial commit

---
+++ ConsoleDemoApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter your firstName:");
+
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("First Name:" + firstName);
+
+            Console.WriteLine("Enter your lastName:");
+
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Last Name:" + lastName);
+
+            Console.WriteLine("Full Name:" + firstName + " " + lastName);
+        }
+    }
+}
