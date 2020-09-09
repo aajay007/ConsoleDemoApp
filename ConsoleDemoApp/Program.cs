3b93b9c986be097af89372f16e497fd65603e5d8
aajay007 <40169124+aajay007@users.noreply.github.com>
2020-09-09 13:01:50 +0530

    first commit

--- ConsoleDemoApp/Program.cs
+++ ConsoleDemoApp/Program.cs
@@ -6,7 +6,6 @@ namespace ConsoleDemoApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
             Console.WriteLine("Enter your firstName:");
 
